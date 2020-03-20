631835f1063da8eafa23b13e552fa267c6811b95
Chankorth <48155332+Chankorth@users.noreply.github.com>
2020-03-20 15:32:31 +0100

    DeleteUpgradeMenu

--- Assets/Generators/generatorBehavior.cs
+++ Assets/Generators/generatorBehavior.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class generatorBehavior : MonoBehaviour
 {
+    private GameObject UI;
+    public GameObject Menu;
     public int Price = 5;
     // Start is called before the first frame update
     void Start()
     {
-        
+        UI = GameObject.Find("UI");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+
+    }
+
+    private void OnMouseOver()
+    {
+    }
+
+    private void OnMouseDown()
+    {
+        GameObject MenuObject = Instantiate(Menu, this.transform.position, UI.transform.rotation);
+        MenuObject.transform.parent = UI.transform;
+        MenuObject.GetComponent<generatorManipulator>().Generator = this.gameObject;
     }
 }
---
+++ Assets/UI/Scripts/generatorManipulator.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class generatorManipulator : MonoBehaviour
+{
+    private UIManager UI;
+    public GameObject Generator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UI = FindObjectOfType<UIManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void deletion()
+    {
+        generatorBehavior behaviour;
+        behaviour = Generator.GetComponent<generatorBehavior>();
+        /*
+         *  UI.Upkeep -= behaviour.Upkeep;
+         *  UI.Energy -= behaviour.Energy;
+         *  UI.PolutionPerRound -= behaviour.PolutionPerRound;
+         *  UI.Happiness -= behaviour.Happiness;
+         */
+        Destroy(this.Generator);
+        Destroy(gameObject);
+    }
+
+    public void upgrade()
+    {
+        generatorBehavior behaviour;
+        behaviour = Generator.GetComponent<generatorBehavior>();
+        // behaviour.Upgrade();
+        Destroy(gameObject);
+    }
+
+}

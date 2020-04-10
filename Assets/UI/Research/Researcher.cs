8d0291345d47311ec2812e15be6d2e0eedec067f
Chankorth <48155332+Chankorth@users.noreply.github.com>
2020-04-10 16:41:46 +0200

    implemented screen

---
+++ Assets/UI/Research/Researcher.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Researcher : MonoBehaviour
+{
+    public bool clicked = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void researched () {
+        this.GetComponent<Image>().color = new Color(0.3443396f, 1, 0.9697767f, 1);
+        clicked = true;
+}
+    public void researchable() {
+        var tempColor = this.GetComponent<Image>().color;
+        tempColor = Color.white;
+        tempColor.a = 100f;
+        this.GetComponent<Image>().color = tempColor;
+    }
+
+
+    public void twoPathsCheck(GameObject button) {
+        if (button.GetComponent<Researcher>().clicked == true)
+        {
+            this.GetComponent<Researcher>().researched();
+        }
+    }
+
+    public void Upgrade1Atom(GameObject Generator) {
+        Generator.GetComponent<generatorBehavior>().Price = 100;
+    }
+}
---
+++ Assets/UI/Research/TypeWriterEffect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TypeWriterEffect : MonoBehaviour
+{
+    public float delay = 0.1f;
+    [TextArea(15, 20)]
+    public string fullText;
+    public GameObject Next;
+
+    private string currentText = "";
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(ShowText());
+    }
+
+    IEnumerator ShowText() {
+        for (int i = 0; i < fullText.Length + 1; i++) {
+            currentText = fullText.Substring(0, i);
+            this.GetComponent<Text>().text = currentText;
+            yield return new WaitForSeconds(delay);
+        }
+    }
+
+    private void Update()
+    {
+        if (fullText.Equals(currentText)) {
+            Next.SetActive(true);
+        }
+    }
+}

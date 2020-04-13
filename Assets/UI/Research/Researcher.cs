05f56141e0dd30ae6416f50eb2b5e35b229b4dd3
Chankorth <48155332+Chankorth@users.noreply.github.com>
2020-04-13 11:26:04 +0200

    added upgrade changes for already placed generators

--- Assets/UI/Research/Researcher.cs
+++ Assets/UI/Research/Researcher.cs
@@ -2,21 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 
 public class Researcher : MonoBehaviour
 {
     public bool clicked = false;
-    // Start is called before the first frame update
-    void Start()
-    {
+    public int PriceAtom = 0;
+    public int UpkeepAtom = 0;
+    public int EnergyAtom = 0;
+    public int PollutionPerRoundAtom = 0;
+    public int HappinessAtom = 0;
 
-    }
+    public int PriceCoal = 0;
+    public int UpkeepCoal = 0;
+    public int EnergyCoal = 0;
+    public int PollutionPerRoundCoal = 0;
+    public int HappinessCoal = 0;
 
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
+    public int PriceWind = 0;
+    public int UpkeepWind = 0;
+    public int EnergyWind = 0;
+    public int PollutionPerRoundWind = 0;
+    public int HappinessWind = 0;
 
     public void researched () {
         this.GetComponent<Image>().color = new Color(0.3443396f, 1, 0.9697767f, 1);
@@ -37,7 +44,11 @@ public class Researcher : MonoBehaviour
         }
     }
 
-    public void Upgrade1Atom(GameObject Generator) {
-        Generator.GetComponent<generatorBehavior>().Price = 100;
+    public void Upgrade1Atom() {
+        GameObject[] generators = (GameObject[]) GameObject.FindObjectsOfType<GameObject>().Where(obj => obj.name == "Atom(Clone)") ;
+        for (int i = 0; i < generators.Length; i++) {
+            generators[i].GetComponent<generatorBehavior>().Price = 100;
+            PriceAtom = 100;
+        }
     }
 }
